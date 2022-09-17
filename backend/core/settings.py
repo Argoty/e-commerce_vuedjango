5164aaaf5d7921614f694a79a2d4c85c66d8b164
JaxisArty <jaxis0810@gmail.com>
2022-09-17 14:22:37 -0500

    product hecho

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -23,6 +23,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 
     "core",
+    "product",
+
     "rest_framework",
     "rest_framework.authtoken",
     "corsheaders",
@@ -108,6 +110,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
