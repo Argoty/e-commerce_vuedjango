012bf28ce120aae630a779dc1880ed472f87100a
JaxisArty <jaxis0810@gmail.com>
2022-09-18 20:40:27 -0500

    listo

--- backend/product/views.py
+++ backend/product/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.http import Http404
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -12,4 +12,14 @@ class LatestProductsList(APIView):
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
 
-# Create your views here.
+class ProductDetail(APIView):
+    def get_object(self, category_slug, product_slug):
+        try:
+            return Product.objects.filter(category__slug=category_slug).get(slug=product_slug)
+        except Product.DoesNotExist:
+            raise Http404
+
+    def get(self, request, category_slug, product_slug, format=None):
+        product = self.get_object(category_slug, product_slug)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
